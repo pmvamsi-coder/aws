576f69452d01b4983c56bd1b8da15db7bd59a2a9
pmvamsi-coder <61499149+pmvamsi-coder@users.noreply.github.com>
2021-05-06 14:55:22 +0530

    sample

--- hi.py
+++ hi.py
@@ -1,7 +1,15 @@
-for i in range(10):
-  print('HI ...vamsi'+str(i))
+print('Vamsi this is ram ')
   
 print('This is written by PM Vamsi')
 
-    
 
+import pandas as pd
+  
+# list of strings
+lst = ['Geeks', 'For', 'Geeks', 'is', 
+            'portal', 'for', 'Geeks']
+  
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst)
+df
+print(df)
